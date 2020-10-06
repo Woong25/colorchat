50809a0e733632fa1c09ff6dae614eafeff6fc31
woong <woong@stunitas.com>
2020-10-06 21:41:19 +0900

    Updated more function

--- app.js
+++ app.js
@@ -4,35 +4,47 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
+const ColorHash = require('color-hash');
 require('dotenv').config();
 
 const webSocket = require('./socket');
 const indexRouter = require('./routes');
+const connect = require('./schemas');
 
 const app = express();
+connect();
+
+const sessionMiddleware = session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+})
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('port', process.env.PORT || 8080);
 //app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile);
+//app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
-
+app.engine('html', require('ejs').renderFile);
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-}));
+app.use(sessionMiddleware);
 app.use(flash());
+app.use((req, res, next) => {
+    if(!req.session.color){
+        const colorHash = new ColorHash();
+        req.session.color = colorHash.hex(req.sessionID);
+    }
+    next();
+});
 
 app.use('/', indexRouter);
 
@@ -45,6 +57,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  console.error(err.stack);
   res.status(err.status || 500);
   res.render('error');
 });
@@ -53,4 +66,4 @@ const server = app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
 });
 
-webSocket(server);
+webSocket(server, app, sessionMiddleware);
--- socket.js
+++ socket.js
@@ -1,57 +1,62 @@
+const axios = require('axios');
 const SocketIO = require('socket.io');
 
-module.exports = (server) => {
+module.exports = (server, app, sessionMiddleware) => {
     const io = SocketIO(server, { path: '/socket.io' });
+    app.set('io', io); // express 변수 저장 [라우터(외부) 에서 io를 사용 하기 위함]
 
-    // 양방향 연결이 되었을 때 (접속)
-    io.on('connection', (socket) => {
+    // 네임스페이스
+    // io.of('/')
+    const room = io.of('/room');
+    const chat = io.of('/chat');
+
+    // 익스프레스 미들웨어를 소켓IO에서 쓰는 방법
+    io.use((socket, next) => { 
+        sessionMiddleware(socket.request, socket.request.res, next);
+    });
+
+    room.on('connection', (socket) => {
+        console.log('room 네임스페이스에 접속');
+        socket.on('disconnect', () => {
+            console.log('room 네임 스페이스 접속 해제')
+        })
+    })
+
+    chat.on('connection', (socket) => {
+        console.log('chat 네임스페이스 접속');
         const req = socket.request;
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log('새로운 클라이언트 접속!', ip, socket.id);
+        const { headers: { referer } } = req;
+        const roomId = referer.split('/')[referer.split('/').length -1].replace(/\?.+/, '');
+
+        socket.join(roomId); // 방에 접속
+
+        socket.to(roomId).emit('join', {
+            user: 'system',
+            chat: `${req.session.color}님이 입장하셨습니다.`,
+        });
 
         socket.on('disconnect', () => {
-            console.log('클라이언트 접속 해제', ip, socket.id);
-            clearInterval(socket.interval);
-        });
+            console.log('chat 네임스페이스 접속 해제');
 
-        socket.on('error', (error) => {
-            console.error(error);
-        });
-        socket.on('reply', (data) => {
-            console.log(data);
-        });
-        socket.interval = setInterval(() => {
-            // send 대신 emit
-            // 키, 값
-            socket.emit('news', 'Hello Socket.IO');
-        }, 3000);
-    });
+            socket.leave(roomId); // 방 나가기
+            const currentRoom = socket.adapter.rooms[roomId];
+            const userCount = currentRoom ? currentRoom.length : 0
+
+            if(userCount === 0){
+                // 라우터를 통해 DB 조작하기
+                axios.delete(`http://localhost:8080/room/${roomId}`)
+                    .then(() => {
+                        console.log('방 제거 요청 성공')
+                    }).catch((err) => {
+                        console.error(err);
+                    });
+            }else{
+                room.emit('changeRoom', {roomId, userCount});
+                socket.to(roomId).emit('exit', {
+                    user: 'system',
+                    chat: `${req.session.color}님이 퇴장하셨습니다.`
+                })
+            }
+        })
+    })
 };
-
-// 양방향 연결이 되었을 때 (접속)
-// wss.on('connection', (ws, req) => {
-//     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-//     console.log('클라이언트 접속 접속.', ip);
-
-//     // 이벤트 명, 콜백 작성
-//     ws.on('message', (message) => {
-//         console.log(message);
-//     });
-//     ws.on('error', (error) => {
-//         console.error(error);
-//     });
-//     ws.on('close', () => {
-//         console.log('클라이언트 접속 해제.', ip);
-//         clearInterval(ws.interval);
-//     });
-
-//     // 서버에서 클라이언트 전송
-//     const interval = setInterval(() => {
-//         // 양방향 연결이 수립 되었을 때만 실행 (에러 방지)
-//         // ws.CONNECTION, ws.CLOSING, ws.CLOSED
-//         if(ws.readyState === ws.OPEN){
-//             ws.send('서버에서 클라이언트로 메세지 전송');
-//         }
-//     }, 3000);
-//     ws.interval = interval;
-// });
